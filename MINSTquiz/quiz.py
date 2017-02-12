680ee181de1b7c770e77c396feb46ba4e7999543
aksagg88 <aksagg88>
2017-02-12 14:33:24 -0500

    added mist dataset

---
+++ MINSTquiz/quiz.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 13:24:29 2017
+
+@author: enterprise
+"""
+
+# Solution is available in the other "quiz_solution.py" tab
+import tensorflow as tf
+
+def get_weights(n_features, n_labels):
+    """
+    Return TensorFlow weights
+    :param n_features: Number of features
+    :param n_labels: Number of labels
+    :return: TensorFlow weights
+    """
+    weights = tf.Variable(tf.truncated_normal((n_features,n_labels)))
+    return weights
+
+
+def get_biases(n_labels):
+    """
+    Return TensorFlow bias
+    :param n_labels: Number of labels
+    :return: TensorFlow bias
+    """
+    biases = tf.Variables(tf.zeros(n_labels))
+    return biases
+
+
+def linear(input, w, b):
+    """
+    Return linear function in TensorFlow
+    :param input: TensorFlow input
+    :param w: TensorFlow weights
+    :param b: TensorFlow biases
+    :return: TensorFlow linear function
+    """
+    # TODO: Linear Function (xW + b)
+    return tf.add(tf.matmul(input, w), b)
